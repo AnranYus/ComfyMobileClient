46b55df4c1a8a63d57fb199afb698dfafc7d4fd0
AnranYus <anranyus@hotmail.com>
2026-05-10 04:57:48 +0800

    chore: add iOS bootstrap project

---
+++ iosApp/iosApp/ContentView.swift
@@ -0,0 +1,19 @@
+import SwiftUI
+import UIKit
+import shared
+
+struct ContentView: View {
+    var body: some View {
+        ComposeView()
+            .ignoresSafeArea(.keyboard)
+    }
+}
+
+struct ComposeView: UIViewControllerRepresentable {
+    func makeUIViewController(context: Context) -> UIViewController {
+        MainViewControllerKt.MainViewController()
+    }
+
+    func updateUIViewController(_ uiViewController: UIViewController, context: Context) {
+    }
+}
